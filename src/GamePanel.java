a1edd6c8a18200d57d51a1e2217d5248cf9cf608
Negarnbkhti42 <norob35@gmail.com>
2020-05-21 15:02:44 +0430

    thread added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,32 @@
 import java.awt.*;
 
-public class GamePanel {
+public class GamePanel implements Runnable {
     private Display display;
 
     private static final String TITLE = "Space invaders";
     private static final int WIDTH = 800, HEIGHT = 600;
 
+    private Thread thread;
+
     public GamePanel(){
         display=new Display(TITLE,WIDTH,HEIGHT);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public synchronized void start(){
+        thread=new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop(){
+        try {
+            thread.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
