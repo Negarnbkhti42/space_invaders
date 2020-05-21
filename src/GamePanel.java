781ac9e42c74437204b77424048f5eef286a3748
Negarnbkhti42 <norob35@gmail.com>
2020-05-21 15:07:11 +0430

    thread added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,22 +7,44 @@ public class GamePanel implements Runnable {
     private static final int WIDTH = 800, HEIGHT = 600;
 
     private Thread thread;
+    private boolean running=false;
 
     public GamePanel(){
         display=new Display(TITLE,WIDTH,HEIGHT);
     }
 
-    @Override
-    public void run() {
+    private void init(){
 
     }
 
+    private void tick(){
+
+    }
+
+    private void render(){
+
+    }
+
+    @Override
+    public void run() {
+        while (running){
+            tick();
+            render();
+        }
+    }
+
     public synchronized void start(){
+        if (running)
+            return;
+        running=true;
         thread=new Thread(this);
         thread.start();
     }
 
     public synchronized void stop(){
+        if (!running)
+            return;
+        running=false;
         try {
             thread.join();
         }catch (InterruptedException e){
